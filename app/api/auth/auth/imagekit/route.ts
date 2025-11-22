f53fba053705f36ff885541e942a6bc713c57353
Eya Abidi <abidi.eya001@gmail.com>
2025-11-22 12:53:42 +0100

    Changed imagekit folder path

--- app/api/auth/auth/imagekit/route.ts
+++ app/api/auth/auth/imagekit/route.ts
@@ -1,18 +1,19 @@
 import ImageKit from "imagekit";
 import config from "@/lib/config";
-import {NextResponse} from "next/server";
+import { NextResponse } from "next/server";
 
+const {
+  env: {
+    imageKit: { publicKey, privateKey, urlEndpoint },
+  },
+} = config;
 
-
-const {env:{imageKit:{publicKey,privateKey,urlEndpoint}}}=config;
-
-const  imagekit = new ImageKit({
+const imagekit = new ImageKit({
   publicKey,
   privateKey,
-  urlEndpoint
+  urlEndpoint,
 });
 
 export async function GET() {
   return NextResponse.json(imagekit.getAuthenticationParameters());
 }
-
